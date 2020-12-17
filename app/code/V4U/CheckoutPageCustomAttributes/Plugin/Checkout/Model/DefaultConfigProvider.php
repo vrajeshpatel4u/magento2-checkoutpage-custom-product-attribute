84dd715bc7e398de064197fb092fb3e0bb81149d
Vrajesh Patel <vrajeshpatel4u@gmail.com>
2020-12-17 12:59:50 +0530

    Custom Product Attributes On Checkout Page

---
+++ app/code/V4U/CheckoutPageCustomAttributes/Plugin/Checkout/Model/DefaultConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace V4U\CheckoutPageCustomAttributes\Plugin\Checkout\Model;
+
+use Magento\Checkout\Model\Session as CheckoutSession;
+
+class DefaultConfigProvider
+{
+    /**
+     * @var CheckoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * Constructor
+     *
+     * @param CheckoutSession $checkoutSession
+     */
+    public function __construct(
+        CheckoutSession $checkoutSession
+    ) {
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    public function afterGetConfig(
+        \Magento\Checkout\Model\DefaultConfigProvider $subject,
+        array $result
+    ) {
+        $items = $result['totalsData']['items'];
+        foreach ($items as $index => $item) {
+            $quoteItem = $this->checkoutSession->getQuote()->getItemById($item['item_id']);
+            $result['quoteItemData'][$index]['manufacturer'] = $quoteItem->getProduct()->getAttributeText('manufacturer');
+        }
+        return $result;
+    }
+}
